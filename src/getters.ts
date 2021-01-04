cde2a21fb3a6e4aa74fcc01cd64981f9d0f993b1
Isaac Simmons <isaac.simmons@sharpspring.com>
2021-01-03 20:58:48 -0500

    Tighten up typescript types, remove 'as unknown'

--- src/getters.ts
+++ src/getters.ts
@@ -4,37 +4,35 @@ import { JsonValue, myParseFloat, myParseInt, parseBigInt, parseBool, parseJson,
 
 //const fooT = <T>(parser: Parser<T>) => ();
 
-// FIXME: can I get rid of the "as unknown" in these?
-// TODO: make all of these params have an optional value of {} (or somethign with required if must be)
 export const fooInt = <T = BasicFieldOptions<number>>(options?: T): T extends OptionalFieldOptions ? FieldOptions<number> & OptionalFieldOptions : FieldOptions<number> =>
   ({
     parser: myParseInt,
     ...options,
-  }) as unknown as T extends OptionalFieldOptions ? FieldOptions<number> & OptionalFieldOptions : FieldOptions<number>;
+  }) as T extends OptionalFieldOptions ? FieldOptions<number> & OptionalFieldOptions : FieldOptions<number>;
 
 export const fooString = <T extends BasicFieldOptions<string>>(options?: T): T extends OptionalFieldOptions ? FieldOptions<string> & OptionalFieldOptions : FieldOptions<string> =>
   ({
     parser: parseString,
     ...options,
-  }) as unknown as T extends OptionalFieldOptions ? FieldOptions<string> & OptionalFieldOptions : FieldOptions<string>;
+  }) as T extends OptionalFieldOptions ? FieldOptions<string> & OptionalFieldOptions : FieldOptions<string>;
 
 export const fooBool = <T extends BasicFieldOptions<boolean>>(options?: T): T extends OptionalFieldOptions ? FieldOptions<boolean> & OptionalFieldOptions : FieldOptions<boolean> =>
   ({
     parser: parseBool,
     ...options,
-  }) as unknown as T extends OptionalFieldOptions ? FieldOptions<boolean> & OptionalFieldOptions : FieldOptions<boolean>;
+  }) as T extends OptionalFieldOptions ? FieldOptions<boolean> & OptionalFieldOptions : FieldOptions<boolean>;
 
 export const fooFloat = <T extends BasicFieldOptions<number>>(options?: T): T extends OptionalFieldOptions ? FieldOptions<number> & OptionalFieldOptions : FieldOptions<number> =>
   ({
     parser: myParseFloat,
     ...options,
-  }) as unknown as T extends OptionalFieldOptions ? FieldOptions<number> & OptionalFieldOptions : FieldOptions<number>;
+  }) as T extends OptionalFieldOptions ? FieldOptions<number> & OptionalFieldOptions : FieldOptions<number>;
 
 export const fooBigInt = <T extends BasicFieldOptions<BigInt>>(options?: T): T extends OptionalFieldOptions ? FieldOptions<BigInt> & OptionalFieldOptions : FieldOptions<BigInt> =>
   ({
     parser: parseBigInt,
     ...options,
-  }) as unknown as T extends OptionalFieldOptions ? FieldOptions<BigInt> & OptionalFieldOptions : FieldOptions<BigInt>;
+  }) as T extends OptionalFieldOptions ? FieldOptions<BigInt> & OptionalFieldOptions : FieldOptions<BigInt>;
 
 // export const getPort = (options: BasicOptionalFieldOptions<number> = {}): OptionalFieldOptions<number> =>
 //   getInt({ min: 0, max: 65535, ...options });
@@ -44,8 +42,8 @@ export const fooBigInt = <T extends BasicFieldOptions<BigInt>>(options?: T): T e
 
 export const fooUrl = <T extends BasicFieldOptions<URL>>(options?: T): T extends OptionalFieldOptions ? FieldOptions<URL> & OptionalFieldOptions : FieldOptions<URL> =>
   ({ parser: parseUrl, ...options,
-  }) as unknown as T extends OptionalFieldOptions ? FieldOptions<URL> & OptionalFieldOptions : FieldOptions<URL>;
+  }) as T extends OptionalFieldOptions ? FieldOptions<URL> & OptionalFieldOptions : FieldOptions<URL>;
 
 export const fooJson = <T extends BasicFieldOptions<JsonValue>>(options?: T): T extends OptionalFieldOptions ? FieldOptions<JsonValue> & OptionalFieldOptions : FieldOptions<JsonValue> =>
   ({ parser: parseJson, ...options,
-  }) as unknown as T extends OptionalFieldOptions ? FieldOptions<JsonValue> & OptionalFieldOptions : FieldOptions<JsonValue>;
+  }) as T extends OptionalFieldOptions ? FieldOptions<JsonValue> & OptionalFieldOptions : FieldOptions<JsonValue>;
